bb1bde8f00dca4b4b7fe10e13208db90243164c4
hirken <silva.ken.97@gmail.com>
2022-02-17 11:23:39 +0900

    fix: JSONを返り値にする

--- src/app/server.go
+++ src/app/server.go
@@ -15,11 +15,26 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", sleep)
+	e.GET("/non", non_sleep)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
+type JsonMessage struct {
+	Message string `json:"message"`
+}
+
 func sleep(c echo.Context) error {
-	time.Sleep(time.Second * 10)
-	return c.String(http.StatusOK, "Hello, World!")
+	time.Sleep(time.Second * 5)
+	data := JsonMessage{
+		Message: "sleep",
+	}
+	return c.JSON(http.StatusOK, data)
+}
+
+func non_sleep(c echo.Context) error {
+	data := JsonMessage{
+		Message: "non_sleep",
+	}
+	return c.JSON(http.StatusOK, data)
 }
